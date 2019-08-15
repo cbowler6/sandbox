829cfba7a19ea25485170d3d6748015894d64c91
cbowler06 <cameron.bowler@my.jcu.edu.au>
2019-08-15 16:28:42 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,12 @@
 """
 Cameron Bowler
 """
+
+MIN_LENGTH = 3
+
+password = input("Enter password:")
+while len(password) < MIN_LENGTH:
+    print("Password must be greater than two letters")
+    password = input("Enter password:")
+
+print("*" * len(password))
